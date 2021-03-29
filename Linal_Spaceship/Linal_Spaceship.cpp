9b300c6102de51e70753dca53ed4e385e830f7a0
TimAvans <38493728+TimAvans@users.noreply.github.com>
2021-03-29 10:59:27 +0200

    Initial commit

---
+++ Linal_Spaceship/Linal_Spaceship.cpp
@@ -0,0 +1,12 @@
+﻿// Linal_Spaceship.cpp : Defines the entry point for the application.
+//
+
+#include "Linal_Spaceship.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
